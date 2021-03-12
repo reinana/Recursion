9d68dcff93a9ed1832f84a1cef05047cea749d42
reina <62531225+reinana@users.noreply.github.com>
2021-03-12 18:13:25 +0900

    Create trumpgame-design3.practice.java

---
+++ trumpgame-design3.practice.java
@@ -0,0 +1,52 @@
+// デッキにあるカードを全て表示するprintDeckメソッドを作成してください。
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Card{
+    public String suit;
+    public String value;
+    public int intValue;
+
+    public Card(String suit, String value, int intValue){
+        this.suit = suit;
+        this.value = value;
+        this.intValue = intValue;
+    }
+
+    public String getCardString(){
+        return this.suit + this.value + "(" + this.intValue + ")";
+    }
+
+}
+class Deck{
+    public ArrayList<Card> deck;
+    
+    public Deck(){
+        this.deck = this.generateDeck();
+    }
+    public static ArrayList<Card> generateDeck(){
+        ArrayList<Card> newDeck = new ArrayList<>();
+        String[] suits = new String[]{"♣", "♦", "♥", "♠"};
+        String[] values = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+        for(int i = 0; i < suits.length; i++){
+            for(int j = 0;j < values.length; j++){
+                // j=0からスタートしているので、最後はj+1にします。トランプに0というカードが存在しないため。
+                newDeck.add(new Card(suits[i], values[j], j + 1));
+            }
+        }
+        return newDeck;
+    }
+
+    // ここから記述してください。
+
+}
+class Main{
+        
+    public static void main(String[] args){
+        Deck deck1 = new Deck();
+        //コンソールで確認
+        deck1.printDeck();  
+    }
+}
