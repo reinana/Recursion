d840bd5c5e213a957b86007f6752a66e876cdde3
reina <62531225+reinana@users.noreply.github.com>
2021-03-12 19:22:12 +0900

    Create trumpgame-design4.practice.java

---
+++ trumpgame-design4.practice.java
@@ -0,0 +1,11 @@
+// 配列intArrの中の数字をシャッフルしてコンソールに表示してみましょう。
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Main{
+    public static void main(String[] args){
+
+        ArrayList<Integer> intArr = new ArrayList<>(Arrays.asList(0,10,20,30,40,50,60,70,80,90,100));
+        // ここから記述してください。 
+    }
+} 
