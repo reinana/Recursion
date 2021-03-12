2c8c7d4f469c69051540d03f1295ead697afb459
reina <62531225+reinana@users.noreply.github.com>
2021-03-12 18:06:46 +0900

    Update trumpgame-design3.java

--- trumpgame-design3.java
+++ trumpgame-design3.java
@@ -11,6 +11,7 @@ class Card{
         this.value = value;
         this.intValue = intValue;
     }
+
     public String getCardString(){
         return this.suit + this.value + "(" + this.intValue + ")";
     }
@@ -23,25 +24,24 @@ class Deck{
         this.deck = this.generateDeck();
     }
     public static ArrayList<Card> generateDeck(){
+        ArrayList<Card> newDeck = new ArrayList<>();
         String[] suits = new String[]{"♣", "♦", "♥", "♠"};
         String[] values = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-        ArrayList<Card> newDeckDinamic = new ArrayList<>();
-        for(int i=0;i<suits.length;i++){
-            for(int j=0;j<values.length;j++){
+        for(int i = 0; i < suits.length; i++){
+            for(int j = 0;j < values.length; j++){
                 // j=0からスタートしているので、最後はj+1にします。トランプに0というカードが存在しないため。
-                newDeckDinamic.add(new Card(suits[i], values[j], j+1));
+                newDeck.add(new Card(suits[i], values[j], j + 1));
             }
         }
-        return newDeckDinamic;
+        return newDeck;
     }
 
-    public String printDeck(){
+    public void printDeck(){
         System.out.println("Displaying cards...");
-        for (Card card: this.deck) {
-                System.out.println(card.getCardString());
+        for (int i = 0; i < this.deck.size(); i++) {
+            System.out.println(this.deck.get(i).getCardString());
         }
-        return "";
     }
 }    
 
@@ -49,6 +49,7 @@ class Main{
         
     public static void main(String[] args){
         Deck deck1 = new Deck();
+        //コンソールで確認
         deck1.printDeck();  
     }
 }
