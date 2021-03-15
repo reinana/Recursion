648e1aba13a1510fea106c97f1916a3daeaeaf24
reina <62531225+reinana@users.noreply.github.com>
2021-03-15 12:48:18 +0900

    Update trumpgame-design10.java

--- trumpgame-design10.java
+++ trumpgame-design10.java
@@ -11,53 +11,54 @@ class Card{
         this.value = value;
         this.intValue = intValue;
     }
+
     public String getCardString(){
         return this.suit + this.value + "(" + this.intValue + ")";
     }
 
 }
+
 class Deck{
     public ArrayList<Card> deck;
     
     public Deck(){
         this.deck = this.generateDeck();
     }
+    
     public static ArrayList<Card> generateDeck(){
+        ArrayList<Card> newDeck = new ArrayList<>();
         String[] suits = new String[]{"♣", "♦", "♥", "♠"};
         String[] values = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-        ArrayList<Card> newDeckDinamic = new ArrayList<>();
-        for(int i=0;i<suits.length;i++){
-            for(int j=0;j<values.length;j++){
-                newDeckDinamic.add(new Card(suits[i], values[j], j+1));
+        for(int i = 0; i < suits.length; i++){
+            for(int j = 0; j < values.length; j++){
+                newDeck.add(new Card(suits[i], values[j], j + 1));
             }
         }
-        return newDeckDinamic;
-    }
-
-    public String printDeck(){
-        System.out.println("Displaying cards...");
-        for (Card card: this.deck) {
-                System.out.println(card.getCardString());
-        }
-        return "";
-    }
-
-    public void shuffleDeck() {
-        for(int i=this.deck.size()-1;i>=0;i--){
-            int j = (int)Math.floor(Math.random() * (i + 1));
-            Card tmp = this.deck.get(i);
-            this.deck.set(i, this.deck.get(j));
-            this.deck.set(j,tmp);
-        }
+        return newDeck;
     }
 
     public Card draw(){
-        Card drawOne = this.deck.remove(this.deck.size()-1);
-        return drawOne;
+        return this.deck.remove(this.deck.size()-1);
     }
-} 
-//プレイヤーの数とゲームモード
+
+    public void printDeck(){
+        System.out.println("Displaying cards...");
+        for (int i = 0; i < this.deck.size(); i++) {
+            System.out.println(this.deck.get(i).getCardString());
+        }
+    }
+
+    public void shuffleDeck() {
+        for(int i = this.deck.size()-1; i >= 0; i--){
+            int j = (int)Math.floor(Math.random() * (i + 1));
+            Card tmp = this.deck.get(i);
+            this.deck.set(i, this.deck.get(j));
+            this.deck.set(j, tmp);
+        }
+    }
+}    
+
 class Table{
     public int amountOfPlayers;
     public String gameMode;
@@ -73,11 +74,12 @@ class Dealer{
         
         Deck deck = new Deck();
         deck.shuffleDeck();
+
         ArrayList<ArrayList<Card>> playerCards = new ArrayList<>();
-        
+ 
         for (int i = 0; i < table.amountOfPlayers; i++) {      
-            ArrayList<Card> playerHand = new ArrayList<Card>(initialCards(table.gameMode));     
-            for (int j = 0; j < initialCards(table.gameMode); j++) {
+            ArrayList<Card> playerHand = new ArrayList<Card>(Dealer.initialCards(table.gameMode));     
+            for (int j = 0; j < Dealer.initialCards(table.gameMode); j++) {
                 Card card1 = deck.draw();
                 playerHand.add(card1);
             }
@@ -92,18 +94,19 @@ class Dealer{
         if (gameMode == "21") return 2;
         else return 0;
     }
-    // 卓の情報を表示するメソッドを作成します。
+
     public static void printTableInformation(ArrayList<ArrayList<Card>> playerCards, Table table) {
-        System.out.println("Amount of players: "+ table.amountOfPlayers +"... Game mode: " + table.gameMode + ". At this table: ");
+        System.out.println("Amount of players: " + table.amountOfPlayers +"... Game mode: " + table.gameMode + ". At this table: ");
         
         for (int i = 0; i < playerCards.size(); i++) {
-            System.out.print("Player " + (i + 1) + " hand is: ");             
+            System.out.println("Player " + (i + 1) + " hand is: ");             
             for(int j = 0; j < playerCards.get(i).size(); j++) {
-                System.out.print(playerCards.get(i).get(j).getCardString());
+                System.out.println(playerCards.get(i).get(j).getCardString());
             }
             System.out.println();
         }            
     }
+
     // 各プレーヤーの手札を受け取って、合計値を計算するscore21Individualメソッドを作成します。
     // ブラックジャックでは値の合計値が21を超えるとNGなのでその場合は0とします。
     public static int score21Individual(ArrayList<Card> cards) {
@@ -113,7 +116,7 @@ class Dealer{
         }
         if (total > 21) total = 0;
         return total;
-    }   
+    }      
 }
 
 class Main{
