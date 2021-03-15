5cf2cdf71fb9632a585abbaab17ca3abbfa76c0b
reina <62531225+reinana@users.noreply.github.com>
2021-03-15 12:20:40 +0900

    Update trumpgame-design9.java

--- trumpgame-design9.java
+++ trumpgame-design9.java
@@ -11,53 +11,54 @@ class Card{
         this.value = value;
         this.intValue = intValue;
     }
+
     public String getCardString(){
         return this.suit + this.value + "(" + this.intValue + ")";
     }
 
 }
+
 class Deck{
     public ArrayList<Card> deck;
     
     public Deck(){
         this.deck = this.generateDeck();
     }
+    
     public static ArrayList<Card> generateDeck(){
+        ArrayList<Card> newDeck = new ArrayList<>();
         String[] suits = new String[]{"♣", "♦", "♥", "♠"};
         String[] values = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-        ArrayList<Card> newDeckDinamic = new ArrayList<>();
-        for(int i=0;i<suits.length;i++){
-            for(int j=0;j<values.length;j++){
-                newDeckDinamic.add(new Card(suits[i], values[j], j+1));
+        for(int i = 0; i < suits.length; i++){
+            for(int j = 0; j < values.length; j++){
+                newDeck.add(new Card(suits[i], values[j], j + 1));
             }
         }
-        return newDeckDinamic;
-    }
-
-    public String printDeck(){
-        System.out.println("Displaying cards...");
-        for (Card card: this.deck) {
-                System.out.println(card.getCardString());
-        }
-        return "";
-    }
-
-    public void shuffleDeck() {
-        for(int i=this.deck.size()-1;i>=0;i--){
-            int j = (int)Math.floor(Math.random() * (i + 1));
-            Card tmp = this.deck.get(i);
-            this.deck.set(i, this.deck.get(j));
-            this.deck.set(j,tmp);
-        }
+        return newDeck;
     }
 
     public Card draw(){
-        Card drawOne = this.deck.remove(this.deck.size()-1);
-        return drawOne;
+        return this.deck.remove(this.deck.size()-1);
     }
-} 
-//プレイヤーの数とゲームモード
+
+    public void printDeck(){
+        System.out.println("Displaying cards...");
+        for (int i = 0; i < this.deck.size(); i++) {
+            System.out.println(this.deck.get(i).getCardString());
+        }
+    }
+
+    public void shuffleDeck() {
+        for(int i = this.deck.size()-1; i >= 0; i--){
+            int j = (int)Math.floor(Math.random() * (i + 1));
+            Card tmp = this.deck.get(i);
+            this.deck.set(i, this.deck.get(j));
+            this.deck.set(j, tmp);
+        }
+    }
+}    
+
 class Table{
     public int amountOfPlayers;
     public String gameMode;
@@ -73,11 +74,12 @@ class Dealer{
         
         Deck deck = new Deck();
         deck.shuffleDeck();
+
         ArrayList<ArrayList<Card>> playerCards = new ArrayList<>();
-        
+ 
         for (int i = 0; i < table.amountOfPlayers; i++) {      
-            ArrayList<Card> playerHand = new ArrayList<Card>(initialCards(table.gameMode));     
-            for (int j = 0; j < initialCards(table.gameMode); j++) {
+            ArrayList<Card> playerHand = new ArrayList<Card>(Dealer.initialCards(table.gameMode));     
+            for (int j = 0; j < Dealer.initialCards(table.gameMode); j++) {
                 Card card1 = deck.draw();
                 playerHand.add(card1);
             }
@@ -87,14 +89,16 @@ class Dealer{
         return playerCards;
     }
 
+
     public static int initialCards(String gameMode) {
         if (gameMode == "poker") return 5;
         if (gameMode == "21") return 2;
         else return 0;
     }
+
     // 卓の情報を表示するメソッドを作成します。
     public static void printTableInformation(ArrayList<ArrayList<Card>> playerCards, Table table) {
-        System.out.println("Amount of players: "+ table.amountOfPlayers +"... Game mode: " + table.gameMode + ". At this table: ");
+        System.out.println("Amount of players: " + table.amountOfPlayers +"... Game mode: " + table.gameMode + ". At this table: ");
         
         for (int i = 0; i < playerCards.size(); i++) {
             System.out.print("Player " + (i + 1) + " hand is: ");             
@@ -109,11 +113,11 @@ class Dealer{
 class Main{
         
     public static void main(String[] args){
-        Table table1 = new Table(2,"21");
+        Table table1 = new Table(3,"21");
         ArrayList<ArrayList<Card>> game1 = Dealer.startGame(table1);
         Dealer.printTableInformation(game1, table1);
 
-        Table table2 = new Table(3,"poker");
+        Table table2 = new Table(4,"poker");
         ArrayList<ArrayList<Card>> game2 = Dealer.startGame(table2);
         Dealer.printTableInformation(game2, table2);     
     }
